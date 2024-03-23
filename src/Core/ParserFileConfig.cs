dfde06aed0e3f8b910ba3641e1f2eeb1f2a8d808
BlueManiac <bluemaniac@users.noreply.github.com>
2024-03-23 12:59:31 +0100

    Changed card back to something more reliable

--- src/Core/ParserFileConfig.cs
+++ src/Core/ParserFileConfig.cs
@@ -9,7 +9,7 @@ public class ParserFileConfig
     public string ResultPath { get; set; } = @"%USERPROFILE%\Documents\My Games\Tabletop Simulator\Saves\Saved Objects\Imported";
     public string ImportPath { get; set; } = "Decks";
     public string CompletedPath { get; set; } = "Completed";
-    public string BackUrl { get; set; } = "https://loremflickr.com/480/680";
+    public string BackUrl { get; set; } = "https://gamepedia.cursecdn.com/mtgsalvation_gamepedia/f/f8/Magic_card_back.jpg?version=0ddc8d41c3b69c2c3c4bb5d72669ffd7";
     public bool CleanDeckNames { get; set; } = true;
     public bool MoveParsedFiles { get; set; } = true;
     public string? ImgurClientKey { get; set; }
