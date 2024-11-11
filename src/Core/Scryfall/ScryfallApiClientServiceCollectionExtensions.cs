6c0bcfcc58107d9274681a36ceff3d9870089a50
BlueManiac <bluemaniac@users.noreply.github.com>
2024-11-11 21:08:40 +0100

    Scryfall: Fixed api access

--- src/Core/Scryfall/ScryfallApiClientServiceCollectionExtensions.cs
+++ src/Core/Scryfall/ScryfallApiClientServiceCollectionExtensions.cs
@@ -5,6 +5,10 @@ public static class ScryfallApiClientServiceCollectionExtensions
 {
     public static void AddScryfallApiClient(this IServiceCollection services)
     {
-        services.AddHttpClient<ScryfallApiClient>(client => client.BaseAddress = new Uri("https://api.scryfall.com/"));
+        services.AddHttpClient<ScryfallApiClient>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.scryfall.com/");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("MTGToTTS/1.0");
+        });
     }
 }
